28d5e94d7111c79331aaa0ad5d1bea57b9cc0940
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2023-07-06 16:22:15 -0300

    Update robo.js

--- robo.js
+++ robo.js
@@ -1,11 +1,12 @@
 //valores achados na tentativa e erro
-//F= 8.86 B = 7.34
+//F = 8.72 B =7.42
 
 const FAST = 3000;
-const SH = 120;
-const SM = 112;
-const SL = 107;
-const SVL = 60;
+const SHL = 126;
+const SH = 123;
+const SM = 115;
+const SL = 100;
+const SVL = 50;
 
 var LSC = 50;
 
@@ -16,7 +17,7 @@ function control(left_sensor, right_sensor, speed) {
     var bre = 0;
     var ste = 0;
     
-    var maxSteering = (speed/130)*(3.14/4)*0.625; //pro robô não sair rolando nas curvas
+    var maxSteering = (speed/130)*(3.14/4)*0.7; //pro robô não sair rolando nas curvas
     
     ste += dif/6;
     if(ste > maxSteering){ste = maxSteering;}
@@ -25,17 +26,17 @@ function control(left_sensor, right_sensor, speed) {
     var eng = FAST - (posDif*5000);
     
     if (speed > SM) {eng -= 450;}
-    if (speed > SM && posDif >= .25) {eng -= 300;}
-    if (speed > SM && posDif >= .5) {eng -= 600;}
+    if (speed > SM && posDif >= .25) {eng -= 700;}
+    if (speed > SM && posDif >= .5) {eng -= 700;}
     
-    if (speed > SH) {eng -= 1500;}
+    if (speed > SH) {eng -= 2250;}
     
-    if(posDif >= .5){LSC = 0;}
+    if(posDif >= .3){LSC = 0;}
     LSC+=1;
     
     //ritual que me faz ganhar tempo
-    var cIn = (4000-(LSC*75));
-    if(cIn < -1009){cIn = -1000;}
+    var cIn = (6000-(LSC*100));
+    if(cIn < -500){cIn = -500;}
     if (LSC > 10){
         eng += cIn;
         console.log(LSC + " : " + cIn);
@@ -43,8 +44,8 @@ function control(left_sensor, right_sensor, speed) {
     //if(LSC > 20){console.log(LSC);}
     
     if (speed < SL) {eng = FAST;}
-    //if (speed < SVL) {eng = FAST+2000;}
-    
+    if (speed < SVL) {eng = FAST+1500;}
+    if (speed > SHL) {eng = 0;}
    
     return {
         engineTorque: eng,
