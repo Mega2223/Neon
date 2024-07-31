c0ceaa4bc90c3c6970c74dbfb5237b8eb0450212
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2024-07-31 16:01:30 -0300

    ué

--- robo.js
+++ robo.js
@@ -1,6 +1,12 @@
 //valores achados na tentativa e erro
 //F = 8,51 B = 7,10
 
+const MS = (3.14/4);
+
+function clamp(min,max,num){
+    return Math.min(Math.max(num,min),max);
+}
+
 function control(left_sensor, right_sensor, speed) {
     
     var dif = right_sensor - left_sensor;
@@ -10,20 +16,22 @@ function control(left_sensor, right_sensor, speed) {
     var eng = 0.0;
     
     var dec = (160.0 - speed)/160.0;
-    console.log(dec);
+    //console.log(dec);
     eng = dec;
+    ste = (dif/2);
     
-    ste = dif/2;
+   
     return {
         engineTorque: eng*5000,
         brakingTorque: bre,
-        steeringAngle: ste * (3.14/4),
+        steeringAngle: ste * MS,
         log: [
             { name: 'Speed', value: speed, min: 0, max: 200 },
             { name: 'Left_sensor', value: left_sensor, min: 0, max: 1 },
             { name: 'Right_sensor', value: right_sensor, min: 0, max: 1 },
             { name: 'Engine Output', value: eng, min: 0, max: 1},
-          
+            { name: 'Steering Output', value: ste, min: -1, max: 1},
+            { name: 'Dif', value: dif, min: -1, max: 1}
         ]
     };
 }
